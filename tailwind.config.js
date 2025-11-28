0e115cad99740d34defce9a0c20d986509f462a6
Hans-Tirta <hansdominict@gmail.com>
2025-11-28 14:17:31 +0700

    Add images for home; update Tailwind config with custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,23 @@ export default {
 
     theme: {
         extend: {
+            colors: {
+                background: {
+                    primary: '#f8ece0',
+                    secondary: '#f3d1b6',
+                },
+                text: {
+                    primary: '#6f3635',
+                    secondary: '#b0a095',
+                    muted: '#cac3ba',
+                },
+                accent: {
+                    red: '#fe6d50',
+                    yellow: '#f99c44',
+                    green: '#92bf5d',
+                    blue: '#7ac2e4',
+                },
+            },
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
